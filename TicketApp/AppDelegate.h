9b15ded5eaa50cf93cf5ba7313cc663d00f931dc
葱花思鸡蛋 <514164510@qq.com>
2017-04-07 09:02:06 +0800

    初始化项目

---
+++ TicketApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicketApp
+//
+//  Created by 葱花思鸡蛋 on 2017/4/6.
+//  Copyright © 2017年 hangzhouzhulongkeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicketApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicketApp
+//
+//  Created by 葱花思鸡蛋 on 2017/4/6.
+//  Copyright © 2017年 hangzhouzhulongkeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
